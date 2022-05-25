a7c1ed982069e7dc8be356a658773592f1c4aaeb
Mateus Franco <71900095+Mateusvff@users.noreply.github.com>
2022-05-24 23:48:15 -0300

    criando operação de instanciação

--- src/main/java/com/example/demo/CursomcApplication.java
+++ src/main/java/com/example/demo/CursomcApplication.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.example.demo.domain.Categoria;
+import com.example.demo.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(cat1);
+		categoriaRepository.save(cat2);
 	}
 
 }
