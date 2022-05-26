da01a5d16a40bd571d591e48dcfb8bf89374fe6c
Mateus Franco <71900095+Mateusvff@users.noreply.github.com>
2022-05-26 14:00:25 -0300

    implementação da classe Produto e instanciação automática

--- src/main/java/com/example/demo/CursomcApplication.java
+++ src/main/java/com/example/demo/CursomcApplication.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.demo.domain.Categoria;
+import com.example.demo.domain.Produto;
 import com.example.demo.repositories.CategoriaRepository;
 
 @SpringBootApplication
@@ -24,8 +27,20 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000);
+		Produto p2 = new Produto(null, "Impressora", 800);
+		Produto p3 = new Produto(null, "Mouse", 80);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.save(cat1);
 		categoriaRepository.save(cat2);
+		
 	}
 
 }
