faebe0cac2aefdfa78038181cf8d27a1e75a4917
Mateus Franco <mateusfrancovinicius@gmail.com>
2022-06-10 14:42:33 -0300

    mapeamento das classes e instanciação de itemPedido

--- src/main/java/com/example/demo/CursomcApplication.java
+++ src/main/java/com/example/demo/CursomcApplication.java
@@ -33,6 +33,8 @@ public class CursomcApplication implements CommandLineRunner{
 	private PagamentoRepository pagamentoRepository;
 	@Autowired
 	private PedidoRepository pedidoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -69,8 +71,19 @@ public class CursomcApplication implements CommandLineRunner{
 		Pagamento pagto1 = new PagamentoComCartão(null, EstadoPagamento.QUITADO, ped1, 6);
 		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
 
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
 		ped1.setPagamento(pagto1);
 		ped2.setPagamento(pagto2);
+
+		ped1.getItens().add(ip1);
+		ped2.getItens().add(ip3);
+
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
 
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
 
@@ -100,8 +113,7 @@ public class CursomcApplication implements CommandLineRunner{
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 
-
-
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 
 	}
 
--- src/main/java/com/example/demo/domain/ItemPedido.java
+++ src/main/java/com/example/demo/domain/ItemPedido.java
@@ -1,27 +1,27 @@
 package com.example.demo.domain;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import java.io.Serializable;
 import java.util.Objects;
 
-@Data
-@NoArgsConstructor
 @Entity
 public class ItemPedido implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @EmbeddedId
-    private ItemPedidoPK id = new ItemPedidoPK(); //chave composta - utiliza uma classe auxiliar
+    private ItemPedidoPK id = new ItemPedidoPK();
 
     private Double desconto;
     private Integer quantidade;
     private Double preco;
 
+    public ItemPedido() {
+    }
+
     public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
+        super();
         id.setPedido(pedido);
         id.setProduto(produto);
         this.desconto = desconto;
@@ -29,24 +29,44 @@ public class ItemPedido implements Serializable {
         this.preco = preco;
     }
 
-    public Pedido getPedido(){
+    public Pedido getPedido() {
         return id.getPedido();
     }
 
-    public Produto getProduto(){
+    public Produto getProduto() {
         return id.getProduto();
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof ItemPedido)) return false;
-        ItemPedido that = (ItemPedido) o;
-        return Objects.equals(getId(), that.getId()) && Objects.equals(getDesconto(), that.getDesconto()) && Objects.equals(getQuantidade(), that.getQuantidade()) && Objects.equals(getPreco(), that.getPreco());
+    public ItemPedidoPK getId() {
+        return id;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getDesconto(), getQuantidade(), getPreco());
+    public void setId(ItemPedidoPK id) {
+        this.id = id;
     }
+
+    public Double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(Double desconto) {
+        this.desconto = desconto;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
 }
