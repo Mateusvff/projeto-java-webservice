2d62fc7c7c3573d8ba3f61e18b923099eb5dce33
Mateus Vinicius Ferreira Franco <Mateus.Franco@cabal.com.br>
2022-06-13 17:30:08 -0300

    Criação do método POST para Categoria

--- src/main/java/com/example/demo/controller/CategoriaResource.java
+++ src/main/java/com/example/demo/controller/CategoriaResource.java
@@ -6,26 +6,42 @@ import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.domain.Categoria;
 import com.example.demo.services.CategoriaService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "categorias")
 public class CategoriaResource {
 
 	@Autowired
-	private CategoriaService service;
+	private CategoriaService categoriaService;
 
-	@GetMapping("categorias")
+	@GetMapping
 	public ResponseEntity<?> getAllCategorias(){
 
-		List<Categoria> allCategorias = service.findAllCategorias();
+		List<Categoria> allCategorias = categoriaService.findAllCategorias();
 		return ResponseEntity.ok().body(allCategorias);
 	}
 
-	@GetMapping("/categorias/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<?> getCategoriaById(@PathVariable Integer id) {
 
-		Categoria obj = service.findCategoriasById(id);
-		return ResponseEntity.ok().body(obj);
+		Categoria categoria = categoriaService.findCategoriasById(id);
+		return ResponseEntity.ok().body(categoria);
 	}
+
+	//inserir uma nova categoria - Enviar um JSON na requisição do tipo POST e salvar no Banco de Dados
+
+	@PostMapping
+	public ResponseEntity<Void> createCategoria(@RequestBody Categoria categoria){
+
+		categoriaService.insertCategoria(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId()).toUri(); //cria uma URI para o novo registro
+		return ResponseEntity.created(uri).build();
+
+	}
+
+
 }
--- src/main/java/com/example/demo/services/CategoriaService.java
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -1,8 +1,10 @@
 package com.example.demo.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import com.example.demo.domain.Produto;
 import com.example.demo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,17 +16,24 @@ import com.example.demo.repositories.CategoriaRepository;
 public class CategoriaService {
 
 	@Autowired
-	private CategoriaRepository repo;
+	private CategoriaRepository categoriaRepository;
 
 	public List<Categoria> findAllCategorias() {
 
-		List<Categoria> allCategorias = repo.findAll();
+		List<Categoria> allCategorias = categoriaRepository.findAll();
 		return allCategorias;
 	}
 
 	public Categoria findCategoriasById(Integer id) { // Find a category by id
-		Optional<Categoria> categorias = repo.findById(id); //Optional may or may not contain a non-null value.
+		Optional<Categoria> categorias = categoriaRepository.findById(id); //Optional may or may not contain a non-null value.
 
 		return categorias.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	//salvar o novo objeto categoria desserializado no banco de dados
+
+	public void insertCategoria(Categoria categoria) {
+		 categoria.setId(null);
+		 categoriaRepository.save(categoria);
 	}
 }
